6d553c8c7444a36235134a9f2b4260208fdf9cbf
parasuramc <parasuramreddy2000@gmail.com>
2025-01-14 02:01:19 +0530

    Added Testdata file and POM structure

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    defaultCommandTimeout: 6000,
-    watchForFileChanges: false,
+    defaultCommandTimeout: 4000,
+    watchForFileChanges: true,
     viewportHeight: 1080,
     viewportWidth: 1920,
+    experimentalStudio:true,
+    pageLoadTimeout: 5000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
